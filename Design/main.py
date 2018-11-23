beefa01eb6f246994fb3146af27914b004bd3449
ezwikda <dd.wiktor@gmail.com>
2018-11-23 16:22:53 +0100

    [task] Initial commit, add welcome note

---
+++ Design/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+print """Welcome adventurer!
+If you see that note it obviously mean that you're interested in seeing some progress regarding this project and machine learning.
+Don't worry, be patient, let's hope few months will be enough to complete this challenge.
+Cheers!
+Neehooy"""
